8a8718893cdf845e17a6e97ae2c7d78fac37fef7
LachlanMurphy <lachlan.g.murphy@gmail.com>
2020-10-10 22:25:12 -0600

    Update tetris.js

--- Tetris/tetris.js
+++ Tetris/tetris.js
@@ -227,7 +227,7 @@ document.addEventListener('keydown', event => {
         playerDrop();
     } else if (event.keyCode === 81) {
         playerRotate(-1);
-    } else if (event.keyCode === 87) {
+    } else if (event.keyCode === 38) {
         playerRotate(1);
     }
 });
