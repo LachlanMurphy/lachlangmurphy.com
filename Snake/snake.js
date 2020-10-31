79a73302593b11bee34d314fa1b7323f934bc49c
LachlanMurphy <lachlan.g.murphy@gmail.com>
2020-10-31 13:01:58 -0600

    Added Mobile Support

--- Snake/snake.js
+++ Snake/snake.js
@@ -1,3 +1,19 @@
+function preventDefault(e) {
+  e.preventDefault();
+}
+
+var supportsPassive = false;
+try {
+  window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+    get: function () { supportsPassive = true; } 
+  }));
+} catch(e) {}
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+window.addEventListener(wheelEvent, preventDefault, wheelOpt);
+window.addEventListener('touchmove', preventDefault, wheelOpt);
+
 let canvas = document.getElementById('gameBoard');
 let context = canvas.getContext('2d');
 context.scale(20, 20);
@@ -122,16 +138,16 @@ document.getElementById('play').onmousedown = function() {
 		}, 150);
 
 		function moveMomentum(event) {
-			if (event.key == 'w' && momentum.y !== 1) {
+			if ((event.key == 'w' || event == 'w') && momentum.y !== 1) {
 				momentum.y = -1;
 				momentum.x = 0;
-			} else if (event.key == 's' && momentum.y !== -1) {
+			} else if ((event.key == 's' || event == 's') && momentum.y !== -1) {
 				momentum.y = 1
 				momentum.x = 0;
-			} else if (event.key == 'd' && momentum.x !== -1) {
+			} else if ((event.key == 'd' || event == 'd') && momentum.x !== -1) {
 				momentum.y = 0;
 				momentum.x = 1;
-			} else if (event.key == 'a' && momentum.x !== 1) {
+			} else if ((event.key == 'a' || event == 'a') && momentum.x !== 1) {
 				momentum.y = 0;
 				momentum.x = -1;
 			}
@@ -146,5 +162,55 @@ document.getElementById('play').onmousedown = function() {
 				}, 150);
 			}
 		}
+
+		document.addEventListener('touchstart', handleTouchStart, false);        
+		document.addEventListener('touchmove', handleTouchMove, false);
+
+		var xDown = null;                                                        
+		var yDown = null;
+
+		function getTouches(evt) {
+		  return evt.touches ||             // browser API
+		         evt.originalEvent.touches; // jQuery
+		}                                                     
+
+		function handleTouchStart(evt) {
+		    const firstTouch = getTouches(evt)[0];                                      
+		    xDown = firstTouch.clientX;                                      
+		    yDown = firstTouch.clientY;                                      
+		};                                                
+
+		function handleTouchMove(evt) {
+		    if ( ! xDown || ! yDown ) {
+		        return;
+		    }
+
+		    var xUp = evt.touches[0].clientX;                                    
+		    var yUp = evt.touches[0].clientY;
+
+		    var xDiff = xDown - xUp;
+		    var yDiff = yDown - yUp;
+
+		    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+		        if ( xDiff > 0 ) {
+		            /* left swipe */ 
+		            moveMomentum('a');
+		        } else {
+		            /* right swipe */
+		            moveMomentum('d')
+		        }                       
+		    } else {
+		        if ( yDiff > 0 ) {
+		            /* up swipe */ 
+		            moveMomentum('w')
+		        } else { 
+		            /* down swipe */
+		            moveMomentum('s');
+		        }                                                                 
+		    }
+		    /* reset values */
+		    xDown = null;
+		    yDown = null;                                             
+		};
 	}
 }
