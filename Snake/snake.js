08124e9a02b3d07bdc8e1906a51f59ff48dc5432
LachlanMurphy <lachlan.g.murphy@gmail.com>
2020-10-30 23:25:16 -0600

    Added end game macanics, and input delay

--- Snake/snake.js
+++ Snake/snake.js
@@ -1,93 +1,111 @@
 let canvas = document.getElementById('gameBoard');
 let context = canvas.getContext('2d');
-
 context.scale(20, 20);
+var gameOn = false;
 
-const arena = createMatrix(25, 25);
+document.getElementById('play').onmousedown = function() {
+	if (gameOn === false) {
+		gameOn = true;
+		document.getElementById('message').innerText ='Snake';
+		localStorage.setItem('inputDelay', '0');
+		var inputDelay = 0;
 
-var momentum = {
-	x: 1,
-	y: 0
-}
+		var arena = createMatrix(25, 25);
 
-class snakeClass {
-	constructor(x, y, value) {
-		this.pos = {
-			x: x,
-			y: y,
+		var momentum = {
+			x: 1,
+			y: 0
 		}
-		this.value = value;
-	}
-}
 
-var snake = [];
-
-for (var i = 0; i < 7; i++) {
-	snake.push(new snakeClass(7 - i, 1, i + 1));
-}
-
-function createMatrix(w, h) {
-    const matrix = [];
-    while (h--) {
-        matrix.push(new Array(w).fill(0));
-    }
-    return matrix;
-}
-
-function drawArena() {
-	arena.forEach((row, x) => {
-		row.forEach((value, y) => {
-			if (value > 0) {
-				context.fillStyle = 'white';
-			} else {
-				context.fillStyle = 'rgb(55, 55, 55)'
+		class snakeClass {
+			constructor(x, y, value) {
+				this.pos = {
+					x: x,
+					y: y,
+				}
+				this.value = value;
 			}
-			context.fillRect(x, y, 1, 1);
-			if (value > 0) {
-				context.fillStyle = 'red';
-				context.fillRect(x + 0.05, y + 0.05, 0.9, 0.9);
-			}
-		});
-	});
-}
-
-function update() {
-	
-	for (var i = 0; i < snake.length; i++) {
-		let snakethis = snake[snake.length - 1 - i];
-		if ((snake[0].pos.x < 24 || momentum.x === -1 || momentum.y !== 0) && (snake[0].pos.y < 24 || momentum.y === -1 || momentum.x !== 0) && (snake[0].pos.x > 0 || momentum.x === 1 || momentum.y !== 0) && (snake[0].pos.y > 0 || momentum.y === 1 || momentum.x !== 0)) {
-			if (i === 0) {
-				arena[snakethis.pos.x][snakethis.pos.y] = 0;
-			}
-			if (snakethis.value !== 1) {
-				snakethis.pos.x = snake[snake.length - 2 - i].pos.x;
-				snakethis.pos.y = snake[snake.length - 2 - i].pos.y;
-			} else {
-				snakethis.pos.x += momentum.x;
-				snakethis.pos.y += momentum.y;
-			}
-			arena[snakethis.pos.x][snakethis.pos.y] = snakethis.value;
 		}
-	}
-	drawArena();
-}
 
-window.setInterval(function() {
-	update();
-}, 150);
+		var snake = [];
 
-document.onkeydown = function(event) {
-	if (event.key == 'w' && momentum.y !== 1) {
-		momentum.y = -1;
-		momentum.x = 0;
-	} else if (event.key == 's' && momentum.y !== -1) {
-		momentum.y = 1
-		momentum.x = 0;
-	} else if (event.key == 'd' && momentum.x !== -1) {
-		momentum.y = 0;
-		momentum.x = 1;
-	} else if (event.key == 'a' && momentum.x !== 1) {
-		momentum.y = 0;
-		momentum.x = -1;
+		for (var i = 0; i < 7; i++) {
+			snake.push(new snakeClass(7 - i, 1, i + 1));
+		}
+
+		drawArena();
+
+		function createMatrix(w, h) {
+		    const matrix = [];
+		    while (h--) {
+		        matrix.push(new Array(w).fill(0));
+		    }
+		    return matrix;
+		}
+
+		function drawArena() {
+			arena.forEach((row, x) => {
+				row.forEach((value, y) => {
+					if (value > 0) {
+						context.fillStyle = 'white';
+					} else {
+						context.fillStyle = 'rgb(55, 55, 55)'
+					}
+					context.fillRect(x, y, 1, 1);
+					if (value > 0) {
+						context.fillStyle = 'red';
+						context.fillRect(x + 0.05, y + 0.05, 0.9, 0.9);
+					}
+				});
+			});
+		}
+
+		function update() {
+			inputDelay += 1;
+			for (var i = 0; i < snake.length; i++) {
+				let snakethis = snake[snake.length - 1 - i];
+				if ((snake[0].pos.x < 24 && snake[0].pos.y < 24 && snake[0].pos.x > 0 && snake[0].pos.y > 0) && !(arena[snake[0].pos.x + momentum.x][snake[0].pos.y + momentum.y] > 0)) {
+					if (i === 0) {
+						arena[snakethis.pos.x][snakethis.pos.y] = 0;
+					}
+					if (snakethis.value !== 1) {
+						snakethis.pos.x = snake[snake.length - 2 - i].pos.x;
+						snakethis.pos.y = snake[snake.length - 2 - i].pos.y;
+					} else {
+						snakethis.pos.x += momentum.x;
+						snakethis.pos.y += momentum.y;
+					}
+					arena[snakethis.pos.x][snakethis.pos.y] = snakethis.value;
+				} else {
+					clearInterval(gameFlow);
+					document.getElementById('message').innerText = 'Game End';
+					gameOn = false;
+				}
+			}
+			drawArena();
+		}
+
+		var gameFlow = window.setInterval(function() {
+			update();
+		}, 150);
+
+		document.onkeydown = function(event) {
+			if (inputDelay !== parseInt(localStorage.getItem('inputDelay'))) {
+				if (event.key == 'w' && momentum.y !== 1) {
+					momentum.y = -1;
+					momentum.x = 0;
+				} else if (event.key == 's' && momentum.y !== -1) {
+					momentum.y = 1
+					momentum.x = 0;
+				} else if (event.key == 'd' && momentum.x !== -1) {
+					momentum.y = 0;
+					momentum.x = 1;
+				} else if (event.key == 'a' && momentum.x !== 1) {
+					momentum.y = 0;
+					momentum.x = -1;
+				}
+				localStorage.setItem('inputDelay', inputDelay);
+			}
+		}
 	}
 }
