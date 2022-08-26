9f37fb0f3badb7bc773a4d8dae066a68da9b7dbc
Lachlan Murphy <lachlan.g.murphy@gmail.com>
2022-08-25 19:08:07 -0600

    Created new home screen
    New home screen and moved around other files to be up to date with current home screen

---
+++ Home/index.js
@@ -0,0 +1,12 @@
+function toggleSidebar() {
+	document.getElementById("sideBar").classList.toggle("active");
+	document.getElementById("burger").classList.toggle("active");
+	document.getElementById("coverDiv").classList.toggle("active");
+	if (document.getElementById("subPieces").classList == "active") {
+		document.getElementById("subPieces").classList.toggle("active");
+	}
+}
+
+function togglePieces() {
+	document.getElementById("subPieces").classList.toggle("active");
+}
