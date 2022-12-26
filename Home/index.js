e03c60d66365bd0415088dc7d754635f7fab4113
Lachlan Murphy <lachlan.g.murphy@gmail.com>
2022-12-26 15:34:18 -0700

    Update index.js

--- Home/index.js
+++ Home/index.js
@@ -13,7 +13,7 @@ function togglePieces() {
 
 // Acount stuff
 // const io = require("socket.io-client");
-const socket = io("https://vps.lachlangmurphy.com", {withCredentials: true});
+const socket = io("https://vps.lachlangmurphy.com");
 
 let user = null;
 if (sessionStorage.getItem('user') !== null) {
